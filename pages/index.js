da095e20d0e95aef48627b4bd419680172588d55
Diogo Verde <diogo.verde05@gmail.com>
2021-07-05 22:15:53 +0100

    added footer and theme sass file

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <main className="flex-grow">
         <Hero />
       </main>
+      <Footer />
     </div>
   );
 }
